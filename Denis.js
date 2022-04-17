0c1e3d769c4c514e7b6d23477a902a392ff031ea
Powerbomq <103332408+Powerbomq@users.noreply.github.com>
2022-04-17 10:38:14 +0200

    Create Denis.js

---
+++ Denis.js
@@ -0,0 +1,1 @@
+{"clientID":"5W+64p55QBHr5McnRqjrNA==","serverToken":"1@BsmjNWkHhpd7/K6RzTQOvf/x83ldoqyrVExbTKZmPUHb0wXQfIhJdU0/NeEKyFFs11YbHwWQzJpgUg==","clientToken":"wNnY9J1vf9U/DFys+rqbgmG+SZjeiaY9MYh6qASgEno=","encKey":"PK+W0TsJoygZokW8fIzdDqCc9C+O63fJSlIuNjJaWww=","macKey":"/3Gksh+NyJEK4Sj930tgM/xel4a1eBCgtqx0sKWDZ8Q="}
